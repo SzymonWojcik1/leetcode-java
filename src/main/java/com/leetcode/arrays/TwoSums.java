274dd7ea27f7ee7e3ecf8f10c43fdd8e2693f33b
Szymon <szymon.wojcik@etu.hesge.ch>
2026-07-21 23:53:21 +0200

    Initial setup

---
+++ src/main/java/com/leetcode/arrays/TwoSums.java
@@ -0,0 +1,10 @@
+package com.leetcode.arrays;
+
+// LeetCode 1 - Two Sum
+// https://leetcode.com/problems/two-sum/
+// Time: O(n), Space: O(n)
+public class TwoSums {
+    public int[] twoSum(int nums, int target) {
+
+    }
+}
